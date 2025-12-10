274eec7e2c9ffe011aec2927010dc19c8445131b
Soham Pal <24052385@kiit.ac.in>
2025-12-10 12:35:47 +0530

    .

---
+++ src/Information.java
@@ -0,0 +1,9 @@
+public class Information {
+    public static void main(String[] args)
+    {
+        System.out.println("Name: Soham Pal");
+        System.out.println("Roll no: 24052385");
+        System.out.println("Section: CSE-16");
+        System.out.println("Branch: CSE");
+    }
+}
